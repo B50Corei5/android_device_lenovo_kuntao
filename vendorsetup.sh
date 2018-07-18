50e0c99a1b4c212d8de47608164ab07aa1c1c620
B50Corei5 <36875492+B50Corei5@users.noreply.github.com>
2018-07-18 13:50:13 +1200

    Prepared for firehound

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo dot_kuntao-eng
-add_lunch_combo dot_kuntao-user
-add_lunch_combo dot_kuntao-userdebug
+add_lunch_combo fh_kuntao-eng
+add_lunch_combo fh_kuntao-user
+add_lunch_combo fh_kuntao-userdebug
