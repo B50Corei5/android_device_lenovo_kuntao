0244d54ac547ba5a42bcf5cf82e24a00c21bcb8e
B50Corei5 <B50Corei5@gmail.com>
2018-07-19 06:01:27 +0000

    Fixed doze errors and prepared for candy

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo lineage_kuntao-eng
-add_lunch_combo lineage_kuntao-user
-add_lunch_combo lineage_kuntao-userdebug
+add_lunch_combo candy_kuntao-eng
+add_lunch_combo candy_kuntao-user
+add_lunch_combo candy_kuntao-userdebug
